211b8825f4ee3136128268f9d9a687de41a9c40b
Fevzi Tugrul Varna <f.varna@sussex.ac.uk>
2022-10-03 02:18:57 +0100

    Update DeletionListActivity.java

--- DeletionListActivity.java
+++ DeletionListActivity.java
@@ -1,7 +1,4 @@
-package uk.ac.westminster.ecwm511.cw2;
-
 import java.util.ArrayList;
-import com.example.coursework2.R;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
